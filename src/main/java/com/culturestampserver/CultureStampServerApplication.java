eacaf2e8bcda0394cf7d167a464b25b10b2d9d45
Jioo Jung <jioome17@gmail.com>
2022-09-25 21:54:30 +0900

    setting

---
+++ src/main/java/com/culturestampserver/CultureStampServerApplication.java
@@ -0,0 +1,13 @@
+package com.culturestampserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultureStampServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CultureStampServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/culturestampserver/CultureStampServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.culturestampserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultureStampServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
