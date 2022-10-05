2720c522d24c725d43a40ff8f54cfb78b3ecd4d2
Jioo Jung <jioome17@gmail.com>
2022-10-05 20:45:56 +0900

    docs:주석

--- src/main/java/com/culturestamp/back/CultureStampBackApplication.java
+++ src/main/java/com/culturestamp/back/CultureStampBackApplication.java
@@ -2,7 +2,7 @@ package com.culturestamp.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
+//hihitest
 @SpringBootApplication
 public class CultureStampBackApplication {
 
