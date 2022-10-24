b7d5a747962826453e53be70dda6c3315bab33ec
Jioo Jung <jioome17@gmail.com>
2022-10-24 16:22:21 +0900

    hi

--- src/main/java/com/culturestamp/back/dto/CategoryResponse.java
+++ src/main/java/com/culturestamp/back/dto/CategoryResponse.java
@@ -6,10 +6,8 @@ import com.culturestamp.back.entity.Category;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoryResponse {
