4108541c311a59adbbba46d401c9a40911bb2fa8
Jeonghee Kim <jeongheekim219@gmail.com>
2022-10-25 00:10:27 +0900

    Feat: User read has been implemented

--- src/main/java/com/culturestamp/back/service/impl/UserServiceImpl.java
+++ src/main/java/com/culturestamp/back/service/impl/UserServiceImpl.java
@@ -1,11 +1,13 @@
 package com.culturestamp.back.service.impl;
 
+import com.culturestamp.back.dto.UserResponse;
 import com.culturestamp.back.entity.User;
 import com.culturestamp.back.repository.UserRepository;
 import com.culturestamp.back.service.UserService;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @NoArgsConstructor
 @Service
@@ -14,9 +16,11 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
-
     @Override
-    public void insertUser(User user) {
-        //userRepository.save()
+    @Transactional(readOnly = true)
+    public UserResponse getUserById(Long userId) {
+        User user = userRepository.findById(userId)
+                                    .orElseThrow(() -> new IllegalStateException("User ID에 해당하는 사용자가 없습니다."));
+        return new UserResponse(user);
     }
 }
