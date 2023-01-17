91bc4eee74374f0638491747866e49f315675fc4
mjheo <ganada7522@naver.com>
2023-01-17 21:02:52 +0900

    cors test

--- src/main/java/com/culturestamp/back/auth/config/web/WebConfig.java
+++ src/main/java/com/culturestamp/back/auth/config/web/WebConfig.java
@@ -1,10 +1,12 @@
 package com.culturestamp.back.auth.config.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebSecurity
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
@@ -17,4 +19,3 @@ public class WebConfig implements WebMvcConfigurer {
             .maxAge(3000);
     }
 }
-
